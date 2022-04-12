f70fb263273018e6089e8c8df641049852f64571
Paul Prestopnik <prestop@ucar.ucar.edu>
2022-04-12 18:33:27 +0000

    Adding printVec helper function

--- chapter1/STL_utilities.cpp
+++ chapter1/STL_utilities.cpp
@@ -1,7 +1,7 @@
 //
 // Created by Paul Prestopnik on 4/12/22.
 //
-//#include <string>
+#include <string>
 #include <iostream>
 #include <vector>
 
@@ -20,11 +20,17 @@ void printVec(const std::string& name, std::vector<T> vec){
 int main(){
 
     std::vector<int> vi = {3,4,5,6,7};
+    printVec("vi", vi);
     PRINTVEC(vi);
+
+    std::vector<std::string> vs = {"this", "is", "a", "test"};
+    PRINTVEC(vs);
 }
 
 /*
  * prestop@asimov:~/git/ral-modern-cxx/chapter1$ g++ -std=c++14 STL_utilities.cpp -o STL_test
  * prestop@asimov:~/git/ral-modern-cxx/chapter1$ ./STL_test
  * vi: 3 4 5 6 7
+ * vi: 3 4 5 6 7
+ * vs: this is a test
  */
