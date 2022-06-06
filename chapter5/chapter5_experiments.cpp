4e897b1f267570cc70d53848bef6db59cc5eb598
Paul Prestopnik <prestop@ucar.ucar.edu>
2022-06-06 00:07:06 +0000

    initial checkin

---
+++ chapter5/chapter5_experiments.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Paul Prestopnik on 6/6/22.
+//
+
+// Back to Basics: Move Semantics (part 1 of 2) - Klaus Iglberger - CppCon 2019
+// https://www.youtube.com/watch?v=St0MNEU5b0o
+
+#include <vector>
+
+std::vector<int> v1 = {1,2,3,4,5};
