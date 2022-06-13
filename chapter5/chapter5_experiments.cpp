bbf48254e6d7dcb8191d68d101e56dd0fd6d29f4
Paul Prestopnik <prestop@ucar.ucar.edu>
2022-06-13 16:00:41 +0000

    trying out some more examples from Back-to-basics on move/forward.

--- chapter5/chapter5_experiments.cpp
+++ chapter5/chapter5_experiments.cpp
@@ -7,8 +7,54 @@
 
 #include <vector>
 
+// https://www.youtube.com/watch?v=pIzaZbKUw2s
+// Back to Basics: Move Semantics (part 2 of 2) - Klaus Iglberger - CppCon 2019
+
+class B {
+public:
+    std::vector<int> vi;
+    void operator()( B&& b){
+        vi = std::move(b.vi);
+    }
+};
+
+template<typename T2>
+class A {
+public:
+    //Example 1
+    template<typename T>
+    void f(T&& t)
+    {
+        b_(std::move(t));
+    }
+
+    void f2(T2&& t)
+    {
+        b_(std::forward<T2>(t));
+    }
+
+    template<typename T>
+    void f3(T&& t)
+    {
+        b_(std::forward<T>(t));
+        b2_(std::forward<T>(t));
+    }
+
+private:
+    B b_;
+    B b2_;
+
+};
+
 int main()
 {
+    A<int> a;
+    B b1;
+    B b2;
+    //a.f3(b1);
+    a.f3(std::move(b2));  // no warning. :(
+
+    /*
     std::vector<int> v1 = {1, 2, 3, 4, 5};
     std::vector<int> v2{};
 
@@ -18,5 +64,9 @@ int main()
 
     // This is invalid!  Clion (Clang-tidy) warns me.
     v1[3] = 5;
+    */
+
+
+
 
 }
